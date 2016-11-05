d9b2d84044284644acb6c1df149dd806b28230e6
Nhat Nguyen Developer <nhatndm1193@gmail.com>
2016-11-06 01:13:03 +0700

    Update

---
+++ cms/jslib/controllers/userctrl.js
@@ -0,0 +1,16 @@
+var userctrl = angular.module("userctrl",[]);
+
+userctrl.controller("usercontroller",function($scope,$http,AuthService,API_ENDPOINT){
+
+     var getinfo = function(){
+        if(AuthService.isAuthenticated()){
+                $http.get(API_ENDPOINT.url + '/api/users/findone/' + AuthService.tokensave()).success(function(response){
+                if(response.success){
+                    $scope.user = response.data;
+                }
+            });
+        }
+    };
+
+    getinfo();
+});
