13675e8c1a514b4c0fc390943d083fa29dc18fd8
Nhat Nguyen Developer <nhatndm1193@gmail.com>
2016-11-07 10:44:04 +0700

    Update GOOGLE API
    Update GOOGLE API

--- cms/jslib/controllers/defaultctrl.js
+++ cms/jslib/controllers/defaultctrl.js
@@ -1,11 +1,24 @@
 var defaultctrl = angular.module('defaultctrl',[]);
 
 defaultctrl.controller("DefaultController",function($scope,$http,AuthService,$window,API_ENDPOINT){
-
+    $scope.isHide = {
+        supperUser : false,
+        user : true,
+        res : true,
+        comment : true,
+        food : true,
+        order : true,
+        message : true,
+        public : true,
+        service : true
+    };
     var getinfo = function(){
         if(AuthService.isAuthenticated()){
                 $http.get(API_ENDPOINT.url + '/api/users/findone/' + AuthService.tokensave()).success(function(response){
                 $scope.name = response.data.local.name;
+                /*if(response.data.role == "SuperUser"){
+                    $scope.isHide.supperUser = false;
+                }*/
             });
         }
     };
@@ -15,5 +28,12 @@ defaultctrl.controller("DefaultController",function($scope,$http,AuthService,$wi
     $scope.logout = function(){
         AuthService.logout();
         $window.location.href = "/";
+    };
+
+    $scope.manageuser = function(){
+        $scope.isHide.user = $scope.isHide.user == true ? false : true ;   
+    };
+    $scope.managerestaurant = function(){
+        $scope.isHide.res = $scope.isHide.res == true ? false : true ;   
     };
 });
